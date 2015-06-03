efa796933d4f444bf3191b3295c1bc28f496da6f
smarbaf <smarbaf@hotmail.com>
2015-06-03 17:25:23 +0100

    Initial Commit

---
+++ features/accept_coins_spec.rb
@@ -0,0 +1,14 @@
+require 'capybara/rspec'
+
+# As a vendor
+# I want a vending machine that accepts coins
+# So that I can collect money from the customer
+
+
+feature 'I want a vending machine which accepts coins' do
+  scenario 'It accepts nickels' do
+    vend = VendingMachine.new
+    vend.insert('nickel')
+    expect(vend.display).to equal(5)
+  end
+end
